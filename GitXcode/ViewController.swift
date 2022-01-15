48166d7924952a3b3cffe50c0604ae90b9120523
vetri <vetriios91@gmail.com>
2022-01-15 22:57:50 +0530

    just a print

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Apple on 15/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello World")
+    }
+
+
+}
+
